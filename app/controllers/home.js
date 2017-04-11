39c982eb4c64ada09965b1b126b764e1a93826e0
Ryan Birmingham <birm@rbirm.us>
2017-04-11 15:00:13 -0400

    add ng-controller hooks

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,6 +2,18 @@ module.controller("Home", ['$scope'], function($scope) {
 
 });
 
+module.controller("Reminder", ['$scope'], function($scope) {
+
+});
+
+module.service('ReminderService', function() {
+    this.data = [["Add Physicians", "10/1/2017"]]
+    this.weather = function() {
+        return this.data
+    }
+    this.
+});
+
 module.controller("ExpandItem", ['$scope'], function($scope) {
 
 });
